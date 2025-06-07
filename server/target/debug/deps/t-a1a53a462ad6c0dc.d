ed0b9f4a428a1d427b813e1449f01c87ea738ee2
谢鹏 <shaipe@qq.com>
2025-06-07 11:18:49 +0800

    chore: remove server/target from remote and add to .gitignore

--- server/target/debug/deps/t-a1a53a462ad6c0dc.d
+++
@@ -1,5 +0,0 @@
-/Users/shaipe/workspace/ui/project/MDocs/server/target/debug/deps/libt-a1a53a462ad6c0dc.rmeta: src/main.rs
-
-/Users/shaipe/workspace/ui/project/MDocs/server/target/debug/deps/t-a1a53a462ad6c0dc.d: src/main.rs
-
-src/main.rs:
--- server/target/debug/deps/t-d1234827e130036f.d
+++
@@ -1,5 +0,0 @@
-/Users/shaipe/workspace/ui/project/MDocs/server/target/debug/deps/libt-d1234827e130036f.rmeta: src/main.rs
-
-/Users/shaipe/workspace/ui/project/MDocs/server/target/debug/deps/t-d1234827e130036f.d: src/main.rs
-
-src/main.rs:
